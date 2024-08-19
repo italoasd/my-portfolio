ec2502d6631ce1d1424d51fd7abdd797b6deff11
italoasd <italosteambr@gmail.com>
2024-08-19 18:07:35 -0300

    update-projects

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,7 +3,7 @@ export function Hero(){
     <>
       <section id="hero">
         <div className="left">
-          <h1>Olá, me chamo <span>Italo</span></h1>
+          <h1>Olá, eu sou <span className="purple">Italo Daniel</span></h1>
         </div>
         <div className="right">
           <a href="https://www.linkedin.com/in/italo-daniel-sa/" target="_blank" rel="noreferrer"><img src="my_icon.png" alt="my icon"  className="icon"/></a>
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,5 @@
-export function Projects(){
-  return(
+export function Projects() {
+  return (
     <>
       <section id="projects">
         <div className="container">
@@ -7,35 +7,55 @@ export function Projects(){
             <h1>Projetos</h1>
           </div>
           <div className="projects-data">
-            <div className="projeto">
-              <div>
-                <h3>em andamento</h3>
+            <div className="info-projeto">
+              <div className="projeto" id="crescendo-na-luta">
+                <img src="projects/crescendo-na-luta.png" alt="imagem do site crescendo na luta" />
+              </div>
+              <div className="descricao-projeto cnl">
+                <h3>Projeto Crescendo na Luta</h3>
+                <p>Projeto desenvolvido para uma iniciativa social, com a intenção de apoiar o projeto e aplicar minhas habilidades na prática.</p>
+                <p className="purple">Tecnologias usadas no projeto: Next.js, TypeScript, HTML, CSS</p>
               </div>
             </div>
-            <div className="projeto">
-              <div>
-                <h3>em andamento</h3>
+            <div className="info-projeto">
+              <div className="projeto">
+                <div>
+                  <h3>em andamento</h3>
+                </div>
               </div>
+              <div className="descricao-projeto"></div>
             </div>
-            <div className="projeto">
-              <div>
-                <h3>em andamento</h3>
+            <div className="info-projeto">
+              <div className="projeto">
+                <div>
+                  <h3>em andamento</h3>
+                </div>
               </div>
+              <div className="descricao-projeto"></div>
             </div>
-            <div className="projeto">
-              <div>
-                <h3>em andamento</h3>
+            <div className="info-projeto">
+              <div className="projeto">
+                <div>
+                  <h3>em andamento</h3>
+                </div>
               </div>
+              <div className="descricao-projeto"></div>
             </div>
-            <div className="projeto">
-              <div>
-                <h3>em andamento</h3>
+            <div className="info-projeto">
+              <div className="projeto">
+                <div>
+                  <h3>em andamento</h3>
+                </div>
               </div>
+              <div className="descricao-projeto"></div>
             </div>
-            <div className="projeto">
-              <div>
-                <h3>em andamento</h3>
+            <div className="info-projeto">
+              <div className="projeto">
+                <div>
+                  <h3>em andamento</h3>
+                </div>
               </div>
+              <div className="descricao-projeto"></div>
             </div>
           </div>
         </div>
