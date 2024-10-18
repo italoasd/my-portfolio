0f09876e753c9b50cc8495ddd4d8612f940b0172
italoasd <italosteambr@gmail.com>
2024-10-18 16:55:24 -0300

    add-links-for-contact

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,12 +1,29 @@
-export function Hero(){
-  return(
+export function Hero() {
+  return (
     <>
       <section id="hero">
         <div className="left">
           <h1>Olá, eu sou <span className="purple">Italo Daniel</span></h1>
+          <div className="buttons-hero">
+            <button className="button-hero">
+              <img src="https://cdn-icons-png.flaticon.com/512/220/220343.png" alt="Linkedin button" />
+              Linkedin
+            </button>
+            <button className="button-hero">
+              <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="Github button" />
+              Github
+            </button>
+            <a href="pdf/Currículo Italo Daniel.pdf" target="_blank">
+              <button className="button-hero">
+                <img src="https://cdn-icons-png.flaticon.com/512/1077/1077035.png" alt="CV button" />
+                Meu CV
+              </button>
+            </a>
+
+          </div>
         </div>
         <div className="right">
-          <a href="https://www.linkedin.com/in/italo-daniel-sa/" target="_blank" rel="noreferrer"><img src="my_icon.png" alt="my icon"  className="icon"/></a>
+          <a href="https://www.linkedin.com/in/italo-daniel-sa/" target="_blank" rel="noreferrer"><img src="my_icon.png" alt="my icon" className="icon" /></a>
         </div>
       </section>
     </>
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,4 +1,36 @@
+import React, { useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
 export function Projects() {
+  // Hook para controlar a animação
+  const controls = useAnimation();
+
+  // Hook para detectar quando o elemento está na viewport
+  const [ref, inView] = useInView({ triggerOnce: true });
+
+  // Iniciar a animação quando o elemento estiver na viewport
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+
+  const projectVariant = {
+    hidden: { opacity: 0, y: 30 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        stiffness: 50,
+        damping: 20,
+        staggerChildren: 0.2, // Espaçamento entre animações de filhos
+        delayChildren: 0.2,   // Delay para o início da animação dos filhos
+      },
+    },
+  };
+
   return (
     <>
       <section id="projects">
@@ -7,63 +39,69 @@ export function Projects() {
             <h1>Projetos</h1>
           </div>
           <div className="projects-data">
-            <div className="info-projeto">
-            <a href="https://projeto-crescendo-na-luta.vercel.app/" target="_blank" rel="noopener noreferrer">
-              <div className="projeto" id="crescendo-na-luta">
-                <img src="projects/crescendo-na-luta.png" alt="imagem do site crescendo na luta" />
-              </div>
-              <div className="descricao-projeto cnl">
-                <h3>Projeto Crescendo na Luta</h3>
-                <p>Projeto desenvolvido para uma iniciativa social, com a intenção de apoiar o projeto e aplicar minhas habilidades na prática.</p>
-                <p className="purple">Tecnologias usadas no projeto: Next.js, TypeScript, HTML, CSS</p>
-              </div>
+            {/* Animação do primeiro projeto */}
+            <motion.div
+              className="info-projeto"
+              ref={ref}
+              initial="hidden"
+              animate={controls}
+              variants={projectVariant}
+              whileInView="visible" // Ativa animação enquanto na viewport
+            >
+              <a
+                href="https://projeto-crescendo-na-luta.vercel.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="projeto" id="crescendo-na-luta">
+                  <img
+                    src="projects/crescendo-na-luta.png"
+                    alt="imagem do site crescendo na luta"
+                  />
+                </div>
+                <div className="descricao-projeto cnl">
+                  <h3>Projeto Crescendo na Luta</h3>
+                  <p>
+                    Projeto desenvolvido para uma iniciativa social, com a
+                    intenção de apoiar o projeto e aplicar minhas habilidades na
+                    prática.
+                  </p>
+                  <p className="purple">
+                    Tecnologias usadas no projeto: Next.js, TypeScript, HTML,
+                    CSS
+                  </p>
+                </div>
               </a>
-            </div>
+            </motion.div>
 
-              <div className="info-projeto">
+           
+            {[...Array(5)].map((_, i) => (
+              <motion.div
+                className="info-projeto"
+                key={i}
+                ref={ref}
+                initial="hidden"
+                animate={controls}
+                variants={projectVariant}
+                whileInView="visible"
+                transition={{
+                  delay: 0.1 * (i + 1), // Aumenta o delay para cada projeto para uma animação sequencial
+                  type: "spring",
+                  stiffness: 50,
+                  damping: 20,
+                }}
+              >
                 <div className="projeto">
                   <div>
                     <h3>em andamento</h3>
                   </div>
                 </div>
                 <div className="descricao-projeto"></div>
-              </div>
-
-            <div className="info-projeto">
-              <div className="projeto">
-                <div>
-                  <h3>em andamento</h3>
-                </div>
-              </div>
-              <div className="descricao-projeto"></div>
-            </div>
-            <div className="info-projeto">
-              <div className="projeto">
-                <div>
-                  <h3>em andamento</h3>
-                </div>
-              </div>
-              <div className="descricao-projeto"></div>
-            </div>
-            <div className="info-projeto">
-              <div className="projeto">
-                <div>
-                  <h3>em andamento</h3>
-                </div>
-              </div>
-              <div className="descricao-projeto"></div>
-            </div>
-            <div className="info-projeto">
-              <div className="projeto">
-                <div>
-                  <h3>em andamento</h3>
-                </div>
-              </div>
-              <div className="descricao-projeto"></div>
-            </div>
+              </motion.div>
+            ))}
           </div>
         </div>
       </section>
     </>
-  )
+  );
 }
